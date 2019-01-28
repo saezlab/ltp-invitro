68636e9cd6e14a6a1ea2e4bb5e742fde08b35ef1
Türei Dénes <turei.denes@gmail.com>
2019-01-28 04:35:57 +0100

    workflow for feature processing and identification

---
+++ workflow_features.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  Copyright (c) 2019 - EMBL
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#
+#  This code is not for public use.
+#  Please do not redistribute.
+#  For permission please contact me.
+#
+#  Website: http://denes.omnipathdb.org/
+#
+
+import imp
+
+from ltp import featureproc
+
+s = featureproc.Screen('invitro')
+s.main()
