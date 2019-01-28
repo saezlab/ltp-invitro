80d392d5b388cca9c9257d6cf87bf2de3ac82397
Türei Dénes <turei.denes@gmail.com>
2019-01-28 18:41:56 +0100

    workflow script

--- workflow_features.py
+++ workflow_features.py
@@ -19,3 +19,25 @@ from ltp import featureproc
 
 s = featureproc.Screen('invitro')
 s.main()
+
+# for testing on one protein:
+#s.set_results_dir()
+#s.read_covariates()
+#s.experiment = ('LCN15', '')
+#s.protein = 'LCN15'
+#s.ionmode = 'pos'
+#s.peak_version = ''
+#s.collect_peaks_files()
+#s.set_sample_id_proc()
+#s.set_ms2_param()
+#s.one_experiment()
+
+# generate profile plots with multiple thresholds
+s = featureproc.Screen(
+    'invitro',
+    do_database_lookup = False,
+    do_ms2 = False,
+    do_export = False,
+    explore_profile_filter = (5.0, 4.0, 3.0, 2.0, 1.5)
+)
+s.main()
