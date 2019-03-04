ab72138f98d83e96ca20a8989543cfb572c64834
Türei Dénes <turei.denes@gmail.com>
2019-03-04 17:26:54 +0100

    workflow for processing curated

---
+++ workflow_postprocess_curated.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+#
+#  Copyright (c) 2019 - EMBL
+#
+#  File author(s): Dénes Türei (turei.denes@gmail.com)
+#
+#  This code is not for public use.
+#  Please do not redistribute.
+#  For permission please contact me.
+#
+#  Website: http://www.ebi.ac.uk/~denes
+#
+
+import imp
+import os
+
+from lipyd import lipproc
+from lipyd import name
+from ltp import results
+from ltp import ltproteins
+
+
+def reload():
+    
+    imp.reload(lipproc)
+    imp.reload(name)
+    imp.reload(ltproteins)
+    imp.reload(results)
+
+
+rr = results.ResultsReader(screen = 'invitro')
+rr.main()
+rr.export()
